c70320f8f4cb42f0f2bb4a3db74c44efef0304e9
Carlos Eduardo da Silva <tadashiestaaqui@gmail.com>
2024-10-24 16:35:45 -0300

    Atualizando o arquivo velocidade.c

--- src/velocidade.c
+++ src/velocidade.c
@@ -3,20 +3,20 @@
     int main() {
         float n1, n2, n3, n4, sub1, sub2, resultado;
 
-    printf("Digite sua velocidade inicial: \n");
+    printf("Digite sua velocidade inicial em Km: \n");
         scanf("%f", &n1);
-    printf("Digite sua velocidade final: \n");
+    printf("Digite sua velocidade final Km: \n");
         scanf("%f", &n2);
 
         sub1 = n1 - n2;
 
-    printf("Digite o tempo inicial: \n");
+    printf("Digite o tempo inicial em horas: \n");
         scanf("%f", &n3);
-    printf("Digite o tempo final: \n");
+    printf("Digite o tempo final em horas: \n");
         scanf("%f", &n4);
 
         sub2 = n3 - n4;
         resultado = sub1 / sub2;
 
-    printf("Sua velocidade media escalar é de: %.1f\n", resultado );
+    printf("Sua velocidade media escalar é de: %.1fKm/h\n", resultado);
     }
