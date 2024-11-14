0c9d32b99e066ef728738088224f3dac5cc053b7
Carlos Eduardo da Silva <tadashiestaaqui@gmail.com>
2024-11-14 14:45:52 -0300

    Que coisa linda

--- src/cadastro.c
+++ src/cadastro.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-    void cadastro(char *nome, char *email, char *idade){
+    void cadastro(char *nome, char *email, int *idade){
 
         FILE *arquivo;
         arquivo = fopen("files/cadastro.txt", "a");
-        fprintf(arquivo, nome);
-        fprintf(arquivo, email);
-        fprintf(arquivo, idade);
+        fprintf(arquivo, "Nome: %s\n", nome);
+        fprintf(arquivo, "Email: %s\n", email);
+        fprintf(arquivo, "Idade: %d\n", *idade);
+        fprintf(arquivo , "------------------------------\n");
         fclose(arquivo);
         printf("Arquivo criado.\n");
     }
@@ -14,14 +15,14 @@
 
         char nome[30];
         char email[50];
-        char idade[3];
+        int idade;
         printf("Boa tarde, digite o seu nome e tecle ENTER.\n");
             scanf("%[^\n]s", nome);
         printf("Agora digite o seu email e tecle ENTER.\n");
             scanf("%s", email);
         printf("Por fim, a sua idade e tecle ENTER.\n");
-            scanf("%s", idade);
-            cadastro(nome, email, idade);
+            scanf("%d", &idade);
+            cadastro(nome, email, &idade);
 
         return 0;
     }
